eb3f2361083c33d327e5171276539f4ab9718167
dabbler2 <117023230+dabbler2@users.noreply.github.com>
2023-11-15 13:21:34 +0900

    move db to rds

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 
 const development = {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PW,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql"
-  }
-  
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+}
+
 module.exports = {development}
